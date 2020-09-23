f775b0c1bc0f3af85cd1e8e3204b1cbfe38a5994
penghuiping <575813261@qq.com>
2020-09-23 16:06:48 +0800

    登入页面

--- app/src/main/java/com/php25/desktop/repostars/config/DbConfig.java
+++ app/src/main/java/com/php25/desktop/repostars/config/DbConfig.java
@@ -3,11 +3,11 @@ package com.php25.desktop.repostars.config;
 import com.php25.common.db.Db;
 import com.php25.common.db.DbType;
 import com.php25.common.db.cnd.JdbcPair;
-import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
+import org.sqlite.SQLiteDataSource;
 
 import javax.sql.DataSource;
 
@@ -18,20 +18,10 @@ import javax.sql.DataSource;
 public class DbConfig {
 
     @Bean
-    public DataSource druidDataSource() {
-        HikariDataSource hikariDataSource = new HikariDataSource();
-        hikariDataSource.setDriverClassName("");
-        hikariDataSource.setJdbcUrl("jdbc:sqlite:repostars.db");
-//        hikariDataSource.setUsername("root");
-//        hikariDataSource.setPassword("root");
-        hikariDataSource.setAutoCommit(true);
-        hikariDataSource.setConnectionTimeout(30000);
-        hikariDataSource.setIdleTimeout(300000);
-        hikariDataSource.setMinimumIdle(1);
-        hikariDataSource.setMaxLifetime(1800000);
-        hikariDataSource.setMaximumPoolSize(15);
-        hikariDataSource.setPoolName("hikariDataSource");
-        return hikariDataSource;
+    public DataSource dataSource() {
+        SQLiteDataSource sqLiteDataSource = new SQLiteDataSource();
+        sqLiteDataSource.setDatabaseName("repostars");
+        return sqLiteDataSource;
     }
 
     @Bean
--- app/src/main/java/com/php25/desktop/repostars/controller/LoginController.java
+++ app/src/main/java/com/php25/desktop/repostars/controller/LoginController.java
@@ -1,14 +1,14 @@
 package com.php25.desktop.repostars.controller;
 
-import com.php25.desktop.repostars.util.GlobalUtil;
 import javafx.fxml.FXML;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,27 +19,37 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoginController {
 
+    @FXML
+    public Button loginBtn;
+    @FXML
+    public Button resetBtn;
+    @FXML
+    public TextField usernameTextField;
+    @FXML
+    public TextField tokenTextField;
+    @FXML
+    public ImageView logo;
+
     @Autowired
     private ConfigurableApplicationContext applicationContext;
 
-    @FXML
-    private Button btn;
 
     @FXML
     public void initialize() {
         try {
-            btn.setText("你好");
-            btn.setOnMouseClicked(mouseEvent -> {
-                log.info("clicked");
-                Stage stage = GlobalUtil.getCurrentStage(mouseEvent);
-                Scene previousScene = stage.getScene();
-                log.info("previousScene:{}", previousScene);
-                Parent load = GlobalUtil.loadFxml("nav_controller.fxml", applicationContext);
-                stage.setScene(new Scene(load, 800, 600));
-                stage.show();
-                Scene latestScene = stage.getScene();
-                log.info("latestScene:{}", latestScene);
-            });
+            logo.setImage(new Image(new ClassPathResource("img/github_logo.png").getInputStream()));
+//            btn.setText("你好");
+//            btn.setOnMouseClicked(mouseEvent -> {
+//                log.info("clicked");
+//                Stage stage = GlobalUtil.getCurrentStage(mouseEvent);
+//                Scene previousScene = stage.getScene();
+//                log.info("previousScene:{}", previousScene);
+//                Parent load = GlobalUtil.loadFxml("nav_controller.fxml", applicationContext);
+//                stage.setScene(new Scene(load, 800, 600));
+//                stage.show();
+//                Scene latestScene = stage.getScene();
+//                log.info("latestScene:{}", latestScene);
+//            });
         } catch (Exception e) {
             throw new RuntimeException("出错啦", e);
         }
